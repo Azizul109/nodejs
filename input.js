89756478ac29bda5fd6190c17e5d91a097f29550
azizularif420 <arif@mazegeek.com>
2024-06-11 10:46:31 +0600

    npm command basic usage

--- input.js
+++ input.js
@@ -9,3 +9,16 @@ rl.question(`What is your name ?`, (name) => {
     console.log(`Hi ${name}`);
     rl.close();
 });
+
+// npm command
+// npm init
+// npm install
+// npm install <package_name> [-g, --save-dev, --no-save, --save-optional, --no-optional]
+// npm install <package_name>@<version>
+// npm update
+// npm update <package_name>
+// npm run <task-name>
+// npm list
+// npm view <package_name> version
+// npm uninstall <package_name>
+// npm help
